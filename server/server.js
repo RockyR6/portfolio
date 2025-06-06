4fa1c70831e4af17e97c4e34985a96601fb199ce
RockyR6 <rockyroy76r@gmail.com>
2025-06-06 08:57:41 +0530

    cors origin frontend url

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["https://portfolio-six-eta-89.vercel.app"],
   credentials: true
 }));
 app.use(express.json());

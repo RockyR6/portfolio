db755b9faf75b52e3d7c886929d4ef991cc1efbc
RockyR6 <rockyroy76r@gmail.com>
2025-06-06 08:46:18 +0530

    backend url fetched

--- client/src/sections/Contact/Contact.jsx
+++ client/src/sections/Contact/Contact.jsx
@@ -35,7 +35,7 @@ function Contact() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/", {
+      const response = await fetch("https://portfoliobackend-woad.vercel.app", {
         method: "POST",
         headers: { 
           "Content-Type": "application/json",
